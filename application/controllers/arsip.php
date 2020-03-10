2d386fb25aa82e2bc055c8eb4ad245b134b38654
Alfian Rochmatul Irman <43021053+alfiannsx98@users.noreply.github.com>
2020-03-10 15:10:35 +0700

    benahin controller arsip

--- application/controllers/arsip.php
+++ application/controllers/arsip.php
@@ -12,6 +12,34 @@ class Arsip extends CI_Controller
     }
     public function index()
     {
-        
+        $data['title'] = 'Arsip';
+        $data['user'] = $this->db->get_where('user', [
+            'email'=> $this->session->userdata('email')
+        ])->row_array();
+
+        $this->form_validation->set_rules('judul', 'Judul', 'required');
+        $this->form_validation->set_rules('abstrak', 'Abstrak', 'required');
+
+        $data['arsip'] = $this->model_arsip->getArsip();
+        $data['getArsipJenis'] = $this->db->get('jenis_arsip')->result_array();
+
+        if ($this->form_validation->run() == false){
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('arsip/index', $data);
+            $this->load->view('templates/footer');
+        }else{
+            $data = [
+                'nim' => ($this->input->post('nim')),
+                'judul' => ($this->input->post('judul')),
+                'abstrak' => ($this->input->post('abstrak')),
+                'keywords' => ($this->input->post('keywords')),
+                'id_jns_arsip' => ($this->input->post('id_jns_arsip'))
+            ];
+            $this->db->insert('arsip', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data Berhasil Ditambahkan</div>');
+            redirect('arsip');
+        }
     }
 }
