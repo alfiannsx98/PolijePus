dffa7dc8202d4cd909bfb483125c834a2ca3f1e3
Alfian Rochmatul Irman <43021053+alfiannsx98@users.noreply.github.com>
2020-03-15 03:11:18 +0700

    Update login_helper.php

--- application/helpers/login_helper.php
+++ application/helpers/login_helper.php
@@ -4,7 +4,7 @@ function is_logged_in()
 {
     $ci = get_instance();
     if (!$ci->session->userdata('email')) {
-        redirect('index.php');
+        redirect('auth');
     } else {
         $role_id = $ci->session->userdata('role_id');
         $menu = $ci->uri->segment(1);
@@ -20,7 +20,7 @@ function is_logged_in()
         ]);
 
         if ($userAccess->num_rows() < 1) {
-            redirect('index.php/auth/blocked');
+            redirect('auth/blocked');
         }
     }
 }
